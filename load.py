6b82780ddb509d875921d5b26260d9c4a4f9a4f2
Lexi Stevens <lexi@stevensfamily.co.uk>
2020-09-10 18:45:32 +0100

    Implement logging setup

--- load.py
+++ load.py
@@ -1,9 +1,11 @@
 # EDMC plugin for Exploration Progress
 import sys
+import os
+import logging
 
 import myNotebook as nb
 from ttkHyperlinkLabel import HyperlinkLabel
-from config import config
+from config import appname, config
 from theme import theme
 
 from system import System
@@ -18,6 +20,19 @@ except ModuleNotFoundError:
     # Python 3
     import tkinter as tk
     import tkinter.ttk as ttk
+
+# Setup logging
+plugin_name = os.path.basename(os.path.dirname(__file__))
+logger = logging.getLogger(f'{appname}.{plugin_name}')
+if not logger.hasHandlers():
+    level = logging.INFO
+    logger.setLevel(level)
+    logger_channel = logging.StreamHandler()
+    logger_formatter = logging.Formatter(f'%(asctime)s - %(name)s - %(levelname)s - %(module)s:%(lineno)d:%(funcName)s: %(message)s')
+    logger_formatter.default_time_format = '%Y-%m-%d %H:%M:%S'
+    logger_formatter.default_msec_format = '%s.%03d'
+    logger_channel.setFormatter(logger_formatter)
+    logger.addHandler(logger_channel)
 
 this = sys.modules[__name__]	# For holding module globals
 
